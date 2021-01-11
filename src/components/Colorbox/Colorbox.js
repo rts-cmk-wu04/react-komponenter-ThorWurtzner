6a8176e25edcc86584deb6f5522ae0d18bedad32
Thor W <6273158@rts-365.dk>
2021-01-11 11:20:00 +0100

    setup

---
+++ src/components/Colorbox/Colorbox.js
@@ -0,0 +1,12 @@
+
+
+function Colorbox(props) {
+    return (
+        <div>
+            <h3>{props.children}</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam aliquam diam sit</p>
+        </div>
+    )
+}
+
+export default Colorbox;
