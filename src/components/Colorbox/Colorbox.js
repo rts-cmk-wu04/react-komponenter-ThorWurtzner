cdab73c7667655e419bc319f6fa056228e66cfb7
Thor W <6273158@rts-365.dk>
2021-01-12 14:41:45 +0100

    finished colorbox component

--- src/components/Colorbox/Colorbox.js
+++ src/components/Colorbox/Colorbox.js
@@ -1,10 +1,13 @@
-
+import "./Colorbox.css"
 
 function Colorbox(props) {
     return (
-        <div>
-            <h3>{props.children}</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam aliquam diam sit</p>
+        <div style={{backgroundColor: props.color}} className="colorbox">
+            <img src={props.icon}></img>
+            <div>
+                <h3>{props.title}</h3>
+                <p>{props.text}</p>
+            </div>
         </div>
     )
 }
