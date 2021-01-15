142f928c74521996a38da0abb76321093732d506
Thor W <6273158@rts-365.dk>
2021-01-15 14:36:32 +0100

    finished product

--- src/App.js
+++ src/App.js
@@ -6,10 +6,22 @@ import Outdoor from './components/Outdoor/Outdoor';
 import Collection from "./components/Collection/Collection";
 import News from './components/News/News';
 import SmallNews from './components/SmallNews/SmallNews';
+import Slider from './components/Slider/Slider';
+import ImageCard from './components/ImageCard/ImageCard';
+import Vr from './components/Vr/Vr';
+import ColorboxSmall from './components/ColorboxSmall/ColorboxSmall';
+import Digest from './components/Digest/Digest';
+import ReadMore from './components/ReadMore/ReadMore';
 
 function App() {
   var [colorboxContent, setColorboxContent] = useState([]);
   var [collectionContent, setCollectionContent] = useState([]);
+  var [newsContent, setNewsContent] = useState([]);
+  var [newsCardsContent, setNewsCardsContent] = useState([]);
+  var [newsCardsBottomContent, setNewsCardsBottomContent] = useState([]);
+  var [colorboxSmallContent, setColorboxSmallContent] = useState([]);
+  var [digestContent, setDigestContent] = useState([]);
+  var [readMoreContent, setReadMoreContent] = useState([]);
 
   useEffect(function() {
     fetch("./Colorbox/colorbox.json")
@@ -21,27 +33,61 @@ function App() {
       })
 
     fetch("./Collection/collection.json")
-    .then(function(response) {
-      return response.json();
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        setCollectionContent(data);
     })
-    .then(function(data) {
-      setCollectionContent(data);
-    })
+
+    fetch("./News/news.json")
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        setNewsContent(data);
+      })
+
+    fetch("./NewsCards/newsCards.json")
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        setNewsCardsContent(data);
+      })
+
+      fetch("./NewsCardsBottom/newsCardsBottom.json")
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        setNewsCardsBottomContent(data);
+      })
+
+      fetch("./ColorboxSmall/colorboxSmall.json")
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        setColorboxSmallContent(data);
+      })
+
+      fetch("./Digest/digest.json")
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        setDigestContent(data);
+      })
+      
+      fetch("./ReadMore/readMore.json")
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        setReadMoreContent(data);
+      })
   }, [])
-
-  var [newsContent, setNewsContent] = useState([]);
-
-    useEffect(function() {
-        fetch("./News/news.json")
-          .then(function(response) {
-            return response.json();
-          })
-          .then(function(data) {
-            setNewsContent(data);
-          })
-      }, [])
-
-     
 
   return (
     <>
@@ -72,9 +118,66 @@ function App() {
       </div>
 
       <div className="newsContainer">
-        <SmallNews />
+        <div>
+          <Slider />
+          <SmallNews />
+          <div className="newsContainerCards">
+            {newsCardsContent.map(content => 
+              <ImageCard image={content.image}
+                         title={content.title}
+                         text={content.text}
+                         color={content.color}
+              />
+            )}
+          </div>
+        </div>
         <News />
       </div>
+
+      <section className="bottom">
+        <div>
+          {newsCardsBottomContent.map(content => 
+              <ImageCard image={content.image}
+                         title={content.title}
+                         text={content.text}
+                         color={content.color}
+              />
+            )}
+          <Vr />
+          <section className="smallCardContainer">
+            {colorboxSmallContent.map(content => 
+              <ColorboxSmall icon={content.icon}
+                             title={content.title} 
+                             color={content.color}
+              />
+            )}
+          </section>
+        </div>
+        <div className="newsDigest">
+          <header>
+            <h1>NEWS DIGEST</h1>
+          </header>
+          <section>
+            {digestContent.map(content => 
+              <Digest color={content.color}
+                      number={content.number}
+                      title={content.title}
+                      text={content.text}
+                      border={content.border}
+              />
+            )}
+          </section>
+        </div>
+        <div>
+          {readMoreContent.map(content =>
+            <ReadMore color={content.color}
+                      border={content.border}
+                      textColor={content.textColor}
+                      text={content.text}
+            />
+          )}
+        </div>
+      </section>
     </>
   );
 }
